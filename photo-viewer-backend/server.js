3ca599be40f2bcec816a8e2cde0b6fa6c575d975
drigrgly <drigrgly@gmail.com>
2026-03-02 02:24:09 +0100

    Creating kubernetes deployment

--- photo-viewer-backend/server.js
+++ photo-viewer-backend/server.js
@@ -5,8 +5,9 @@ const cookieParser = require("cookie-parser");
 const app = express();
 const cors = require('cors');
 const port = 3000;
+const path = require('path');
 
-require('dotenv').config({path:'./.ENV'})
+require('dotenv').config({path:'./.env'})
 
 app.use(express.json());
 app.use(express.urlencoded());
@@ -25,12 +26,12 @@ app.use((err, req, res, next) => {
   return res.status(401).send({error: err.message});
 })
 
-app.use('/{*any}', 
-  (req, res, next) => {
-    res.send('There is nothing here');
-  }
-);
+let distFolder = path.join(__dirname, "client/photo-viewer-frontend/browser")
+app.use(express.static(distFolder));
 
+app.get("/{*any}", (req, res) => {
+  res.sendFile(path.join(distFolder, "index.html"))
+})
 app.listen(port, () => {
   console.log(`Web server running on port ${port}`);
 });
